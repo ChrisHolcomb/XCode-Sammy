ce8450711d1704e4f119eebd830484d7a169c87c
Chris Holcomb <chris@pooldashboard.com>
2019-10-24 14:58:06 -0500

    Initial Commit

---
+++ Sammy-School-Projects/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sammy-School-Projects
+//
+//  Created by Chris Holcomb on 10/24/19.
+//  Copyright © 2019 Chris Holcomb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
